b62a8ad1d2ae1348a2f36968a7ed6f865843faef
check96 <donadio24@gmail.com>
2019-05-21 11:34:24 +0200

    Create mazeActions.pl

---
+++ Pozzato/mazeActions.pl
@@ -0,0 +1,18 @@
+% applicabile(Azione,Stato).
+
+applicabile(est,pos(R,C)) :- numColonne(NC), C < NC, Colonna is C+1, \+occupata(pos(R,Colonna)).
+applicabile(ovest,pos(R,C)) :- C>1, Colonna is C-1, \+occupata(pos(R,Colonna)).
+applicabile(nord,pos(R,C)) :- R>1, Riga is R-1, \+occupata(pos(Riga,C)).
+applicabile(sud,pos(R,C)) :- numRighe(NR), R < NR, Riga is R+1, \+occupata(pos(Riga,C)).
+
+% trasforma(Azione,Stato,NuovoStato).
+
+trasforma(est,pos(Riga,Colonna),pos(Riga,ColonnaAccanto),1) :- ColonnaAccanto is Colonna+1.
+trasforma(ovest,pos(Riga,Colonna),pos(Riga,ColonnaAccanto),1) :- ColonnaAccanto is Colonna-1.
+trasforma(nord,pos(Riga,Colonna),pos(RigaSopra,Colonna),1) :- RigaSopra is Riga-1.
+trasforma(sud,pos(Riga,Colonna),pos(RigaSotto,Colonna),1) :- RigaSotto is Riga+1.
+
+% distanza dal nodo finale
+distanza(pos(X1,Y1),D) :-
+    finale(pos(X2,Y2)),
+    D is ((X2-X1)**2 + (Y2-Y1)**2).

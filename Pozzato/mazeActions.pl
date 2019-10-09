9fa68f83bd6d0cd39259469f508cfab01f8b195f
check96 <donadio24@gmail.com>
2019-10-09 19:40:43 +0200

    change actions e create new maze

--- Pozzato/mazeActions.pl
+++ Pozzato/mazeActions.pl
@@ -12,7 +12,12 @@ trasforma(ovest,pos(Riga,Colonna),pos(Riga,ColonnaAccanto),1) :- ColonnaAccanto 
 trasforma(nord,pos(Riga,Colonna),pos(RigaSopra,Colonna),1) :- RigaSopra is Riga-1.
 trasforma(sud,pos(Riga,Colonna),pos(RigaSotto,Colonna),1) :- RigaSotto is Riga+1.
 
-% distanza dal nodo finale
-distanza(pos(X1,Y1),D) :-
-    finale(pos(X2,Y2)),
-    D is ((X2-X1)**2 + (Y2-Y1)**2).
+
+% distanza tra due nodi
+distanza(pos(X1,Y1),D):-
+  finale(pos(X2,Y2)),
+  D is ((X2-X1)**2 + (Y2-Y1)**2).     % distanza cartesiana
+  %abs(X2-X1, X), abs(Y2-Y1, Y), D is X + Y.   % distanza di Manhattan
+
+abs(X,X) :- X >= 0, !.
+abs(X,-X).

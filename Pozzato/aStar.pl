233c5b2763edbcfefa27eae82e7e74dd90c88ce8
check96 <donadio24@gmail.com>
2019-11-05 16:35:05 +0100

    fix generaFigli bugs

--- Pozzato/aStar.pl
+++ Pozzato/aStar.pl
@@ -2,45 +2,45 @@
 
 search(Soluzione) :-
   iniziale(S),
-  aStar([nodo(S,0,[])],nodo(S,0,[]),[],Sol), reverse(Sol,Soluzione),length(Soluzione,L),write(L),nl.
+  aStar([nodo(S,0,[])],nodo(S,0,[]),[],Sol), reverse(Sol,Soluzione).
 
 % aStar(open(nodo(posizione,valore,azioni)), nodeMin, closed, soluzione)
-aStar(_,nodo(S,_,Azioni),_,Azioni) :- finale(S), !.                                          % restituisco la soluzione (le azioni), se il prossimo nodo da espandere è un nodo finale S
+aStar(_,nodo(S,_,Azioni),_,Azioni) :- finale(S), !.                                     % restituisco la soluzione (le azioni), se il prossimo nodo da espandere è un nodo finale S
 aStar(Open,nodo(NodeMin,ValueMin,Actions), Close,Soluzione):-
-    findall(Azione,applicabile(Azione,NodeMin),ListaApplicabili),                               % cerco tutte le possibili azioni applicabili al nodo NodeMin
-    generaFigli(nodo(NodeMin,ValueMin,Actions),ListaApplicabili,ListaFigli,Open,Close),         % eseguo le azioni
-    select(nodo(NodeMin,ValueMin,Actions),Open,NewOpen),                                        % elimino NodeMin dalla lista Open
-    append(NewOpen,ListaFigli,NuovaCoda),                                                       % aggiungo a open la lista dei figli generati precedentemente
-    min(NuovaCoda,NewNodeMin),                                                                  % calcolo il nuovo nodeMin, cioè il nodo con l'euristica migliore (distanza minore), tra quelli presenti in NuovaCoda
-    aStar(NuovaCoda,NewNodeMin,[NodeMin|Close],Soluzione).                                      % aggiungo NodeMin alla lista dei visitati e continuo l'esplorazione
+    findall(Azione,applicabile(Azione,NodeMin),ListaApplicabili),                       % cerco tutte le possibili azioni applicabili al nodo NodeMin
+    generaFigli(nodo(NodeMin,ValueMin,Actions),ListaApplicabili,List,Open,Close),       % eseguo le azioni, aggiungendo i nodi alla lista Open
+    select(nodo(NodeMin,ValueMin,Actions),List,NewOpen),                                % elimino NodeMin dalla lista Open
+    min(NewOpen,NewNodeMin),                                                            % calcolo il nuovo nodeMin, cioè il nodo con l'euristica migliore (distanza minore), tra quelli presenti in NuovaCoda
+    aStar(NewOpen,NewNodeMin,[NodeMin|Close],Soluzione).                                % aggiungo NodeMin alla lista dei visitati e continuo l'esplorazione
 
 
-% generaFigli(Nodo(nodo,valore,azioni),ListaApplicabili,ListaFigli,Open,Close).
-generaFigli(_,[],[],_,_).
+% generaFigli(Nodo(nodo,valore,azioni),ListaApplicabili,NewOpen,Open,Close).
+generaFigli(_,[],Open,Open,_).
 
-generaFigli(nodo(S,Value,AzioniPerS),[Azione|AltreAzioni],[nodo(SNuovo,NewValue,[Azione|AzioniPerS])|FigliTail],Open,Close):-
-    trasforma(Azione,S,SNuovo,Costo),                                             % eseguo Azione e creo un nodo in una nuova posizione SNuovo
-    NewValue is Costo + Value,                                                    % calcolo il valore dell'euristica del nuovo nodo
-    checkOpen(nodo(SNuovo,NewValue,[Azione|AzioniPerS]),Open, NewOpen),           % controllo se il nodo S è presente nella lista open. vedi sotto
-    \+member(SNuovo,Close),!,                                                     % controllo se SNuovo non fa parte dei nodi già visitati
-    generaFigli(nodo(S,Value,AzioniPerS),AltreAzioni,FigliTail,NewOpen,Close).    % eseguo le azioni rimanenti
+% se tutti i controlli vanno a buon fine nella chiusura della chiamata ricorsiva aggiunge SNuovo a newOpen
+generaFigli(nodo(S,Value,AzioniPerS),[Azione|AltreAzioni],[nodo(SNuovo,NewValue,[Azione|AzioniPerS])|NewOpen],Open,Close):-
+    trasforma(Azione,S,SNuovo,Costo),                                        % eseguo Azione e creo un nodo in una nuova posizione SNuovo
+    NewValue is Costo + Value,                                               % calcolo il valore del costo di cammino del nuovo nodo
+    \+member(SNuovo,Close),                                                  % controllo se SNuovo fa parte dei nodi già visitati
+    checkOpen(nodo(SNuovo,NewValue,[Azione|AzioniPerS]),Open, List),!,       % controllo se il nodo S è presente nella lista open. vedi sotto
+    generaFigli(nodo(S,Value,AzioniPerS),AltreAzioni,NewOpen,List,Close).    % eseguo le azioni rimanenti
 
 % se uno dei controlli è andato male, scarto l'azione ed eseguo le rimanenti azioni
-generaFigli(nodo(S,Value,AzioniPerS),[_|AltreAzioni],FigliTail,Open,Close):-
-    generaFigli(nodo(S,Value,AzioniPerS),AltreAzioni,FigliTail,Open,Close).
+generaFigli(nodo(S,Value,AzioniPerS),[_|AltreAzioni],NewOpen,Open,Close):-
+    generaFigli(nodo(S,Value,AzioniPerS),AltreAzioni,NewOpen,Open,Close).
 
 /* controlla se il nodo S è presente nella lista open.
    restiuisce:
    - la lista open se il nodo non è presente in essa
-   - una nuova lista identica a open ma con il nodo aggiornato, se il nodo è presente nella lista ma con un valore più alto
+   - la lista open senza il nodo, se questo è presente nella lista ma con un valore più alto
    - false altrimenti.
 
    checkOpen(Nodo,Open,NewOpen)
 */
 % se la lista è vuota, restituisce la lista vuota. CASO BASE
 checkOpen(_,[],[]) :- !.
-% se il nodo è presente nella lista ma con un valore maggiore di quello del nuovo nodo, aggiorno il nodo e restituisco la lista aggiornata.
-checkOpen(nodo(S,Value,Azioni), [nodo(S,ListValue,_)|Tail], [nodo(S,Value,Azioni)|Tail]) :-
+% se il nodo è presente nella lista ma con un valore maggiore di quello del nuovo nodo, non lo aggiungo alla nuova lista (cosi facendo di fatto lo eliminiamo dalla lista Open)
+checkOpen(nodo(S,Value,_), [nodo(S,ListValue,_)|Tail], Tail) :-
   Value < ListValue, !.
 % altrimenti se è presente ma con un valore minore o uguale, restituisco false.
 checkOpen(nodo(S,_,_), [nodo(S,Value,Actions)|Tail], [nodo(S,Value,Actions)|Tail]) :- !, false.
